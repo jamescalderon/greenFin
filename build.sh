d37742c2c3798bfc70da67deaedd6f8a77829355
James Calderon <jamescalderon@users.noreply.github.com>
2024-05-07 15:34:07 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ echo "USER = $USER"
 curl -o /etc/yum.repos.d/refi64-webapp-manager-fedora.repo "https://copr.fedorainfracloud.org/coprs/refi64/webapp-manager/repo/fedora-${RELEASE}/refi64-webapp-manager-fedora-${RELEASE}.repo"
 
 # - Firefox PWA
-sudo rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
+rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
 
 echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | sudo tee /etc/yum.repos.d/firefoxpwa.repo
 
