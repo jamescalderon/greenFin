72ee26a4f37088ba756131911644118dbce343ff
James Calderon <jamescalderon@users.noreply.github.com>
2024-05-16 14:11:13 -0400

    gnome-terminal-nautilus

--- build.sh
+++ build.sh
@@ -39,8 +39,8 @@ rpm-ostree install nautilus-open-any-terminal
 rpm-ostree install jetbrains-mono-fonts
 rpm-ostree install python3-pip
 rpm-ostree install gparted
-rpm-ostree install grub-customizer \
-gnome-terminal-nautilus
+rpm-ostree install grub-customizer
+rpm-ostree install gnome-terminal-nautilus
 
 # from COPRs:
 rpm-ostree install firefoxpwa
